438b79b3d182f4a59ad36c775cf8d020403dc3cf
JuanQuiroz <jaqp_911@hotmail.com>
2021-08-16 01:27:26 -0500

    test

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   reactStrictMode: true,
-}
+  trailingSlash: true,
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+    };
+  },
+
+  images: {
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  },
+};
