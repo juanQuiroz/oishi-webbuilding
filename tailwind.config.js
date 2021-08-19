7539897ea6ad29cd2f95564a0e7300dd8d94c732
JuanQuiroz <jaqp_911@hotmail.com>
2021-08-18 21:04:41 -0500

    responsive add

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ["ui-sans-serif", "system-ui"],
+      serif: ["ui-serif", "Georgia"],
+      mono: ["ui-monospace", "SFMono-Regular"],
+      display: ["Oswald"],
+      body: ['"Open Sans"'],
+      oishi: ['"Andika New Basic"'],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
